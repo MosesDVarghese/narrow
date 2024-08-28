24835814e281abe6522c523d1fb8538f6a947d7d
MosesDVarghese <accydent@gmail.com>
2024-08-28 16:53:48 +0400

    modify solution

--- src/solutions/math/p412_fizz_buzz.rs
+++ src/solutions/math/p412_fizz_buzz.rs
@@ -22,37 +22,51 @@ pub struct Solution;
 
 impl Solution {
     pub fn fizz_buzz(n: i32) -> Vec<String> {
-        // define return array
-        let mut answer: Vec<String> = Vec::new(); 
+        // // define return array
+        // let mut answer: Vec<String> = Vec::new(); 
 
-        // check for negatives
-        if n <= 0 {
-            println!("Invalid input! Number is not positive!");
-            return answer;
-        }
+        // // check for negatives
+        // if n <= 0 {
+        //     return answer;
+        // }
 
+        // // loop from 1 to the number
+        // for i in 1..=n {
+        //     // get the number
+        //     println!("Number: {}", i);
 
-        // loop from 1 to the number
+        //     // if the number[i] is divisible by 3 and 5 -> fizzbuzz
+        //     if i % 3 == 0 && i % 5 == 0 {
+        //         answer.push("FizzBuzz".to_string());
+        //     }
+        //     // if the number[i] is divisible by 3 -> fizz
+        //     else if i % 3 == 0 {
+        //         answer.push("Fizz".to_string());
+        //     }
+        //     // if the number[i] is divisible by 5 -> buzz
+        //     else if i % 5 == 0 {
+        //         answer.push("Buzz".to_string());
+        //     }
+        //     else {
+        //         answer.push(i.to_string());
+        //     }
+        // }
+
+        // answer
+
+        // alternate solution:
+
+        // define the return array
+        let mut answer: Vec<String> = Vec::with_capacity(n as usize);
+
         for i in 1..=n {
-            // get the number
-            println!("Number: {}", i);
-
-            // if the number[i] is divisible by 3 and 5 -> fizzbuzz
-            if i % 3 == 0 && i % 5 == 0 {
-                answer.push("FizzBuzz".to_string());
-            }
-            // if the number[i] is divisible by 3 -> fizz
-            else if i % 3 == 0 {
-                answer.push("Fizz".to_string());
-            }
-            // if the number[i] is divisible by 5 -> buzz
-            else if i % 5 == 0 {
-                answer.push("Buzz".to_string());
-            }
-            else {
-                answer.push(i.to_string());
-            }
-
+            // push with match statement
+            answer.push(match(i % 3, i % 5) {
+                (0, 0) => "FizzBuzz".to_string(),
+                (0, _) => "Fizz".to_string(),
+                (_, 0) => "Buzz".to_string(),
+                _ => i.to_string(),
+            })
         }
 
         answer
@@ -63,8 +77,8 @@ pub fn run() {
     let info: ProblemInfo = ProblemInfo::new(NUMBER, &NAME, &VARIANT, &TAKEAWAY);
     info.display();
 
-    // let n: i32 = 15;
-    let n: i32 = -33;
+    let n: i32 = 15;
+    // let n: i32 = -33;
     // let n: i32 = -1111222119;
     let answer: Vec<String> = Solution::fizz_buzz(n);
     println!("Result: {:?}", answer);
