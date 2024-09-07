8c6c9e78d0e0bb6218c50bc390dabe37b5680268
MosesDVarghese <accydent@gmail.com>
2024-09-07 23:02:03 +0400

    add n_primes

--- src/misc/p201.rs
+++ src/misc/p201.rs
@@ -16,14 +16,14 @@ use std::usize;
 use crate::utils::problem_info::ProblemInfo;
 
 const NUMBER: u16 = 5001;
-const NAME: &str = "Sieve of Eratosthenes";
+const NAME: &str = "Sieve of Eratosthenes | Primes";
 const VARIANT: &str = "Unknown";
 const TAKEAWAY: &str = "it's simpler than others make it seem";
 
 pub struct Solution;
 
 impl Solution {
-    pub fn n_primes(n: i32) -> Vec<i32> {
+    fn primes_less_than(n: i32) -> Vec<i32> {
         let mut sieve = vec![1; n as usize];
         let mut res = Vec::new();
 
@@ -41,6 +41,29 @@ impl Solution {
 
         res
     }
+
+    fn n_primes(n: i32) -> Vec<i32> {
+        let mut res: Vec<i32> = Vec::new();
+        res.push(2);
+        let mut num = 3;
+
+        while (res.len() as i32) < n {
+            let mut is_prime: bool = true;
+            for i in 0..res.len() {
+                if num % res[i] == 0 {
+                    is_prime = false;
+                    break;
+                }
+            }
+            if is_prime {
+                res.push(num);
+            }
+
+            num += 2;
+        }
+
+        res
+    }
 }
 
 pub fn run() {
@@ -48,8 +71,6 @@ pub fn run() {
     info.display();
 
     let n: i32 = 15;
-    // let n: i32 = -33;
-    // let n: i32 = -1111222119;
     let res: Vec<i32> = Solution::n_primes(n);
     println!("Result: {:?}", res);
 
